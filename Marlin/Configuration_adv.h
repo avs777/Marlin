fb2f7fc9f9d0ee5a6ef8d060ecec1e0162211606
Aleksey Scherbak <alex_scherbak@hotmail.com>
2026-01-21 08:54:07 -0800

    handle K in g-code

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2448,7 +2448,7 @@
   #if ENABLED(DISTINCT_E_FACTORS)
     #define ADVANCE_K { 0.22 }    // (mm) Compression length per 1mm/s extruder speed, per extruder. Override with 'M900 T<tool> K<mm>'.
   #else
-    #define ADVANCE_K 0.05        // (mm) Compression length for all extruders. Override with 'M900 K<mm>'.   // AVS
+    #define ADVANCE_K 0.00        // (mm) Compression length for all extruders. Override with 'M900 K<mm>'.   // AVS - handle K in g-code
   #endif
   //#define ADVANCE_K_EXTRA       // Add a second linear advance constant, configurable with 'M900 L'.
 #endif
