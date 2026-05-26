0d9ba330aec29ecc2e06b924fc24bb868273c9b8
Aleksey Scherbak <alex_scherbak@hotmail.com>
2026-05-25 17:27:41 -0700

    version update

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "avs v2.1.x-20260316" // AVS
+  #define SHORT_BUILD_VERSION "avs v2.1.x-20260525" // AVS
 #endif
 
 /**
